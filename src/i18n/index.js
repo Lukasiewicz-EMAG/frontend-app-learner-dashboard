04251752156a925aa5ae88790baf8d116dda15ba
Wojciech Górka | Łukasiewicz – EMAG <wojciech.gorka@emag.lukasiewicz.gov.pl>
2024-09-06 12:08:14 +0200

    polish translation for redwood

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,1 +1,25 @@
-export default [];
+import { messages as footerMessages } from '@edx/frontend-component-footer';
+
+import arMessages from './messages/ar.json';
+// no need to import en messages-- they are in the defaultMessage field
+import es419Messages from './messages/es_419.json';
+import frMessages from './messages/fr.json';
+import frcaMessages from './messages/fr_CA.json';
+import ptbrMessages from './messages/pt_BR.json';
+import zhcnMessages from './messages/zh_CN.json';
+import plMessages from './messages/pl.json';
+
+const appMessages = {
+  ar: arMessages,
+  'es-419': es419Messages,
+  'pl': plMessages,
+  fr: frMessages,
+  'fr-ca': frcaMessages,
+  'pt-br': ptbrMessages,
+  'zh-cn': zhcnMessages,
+};
+
+export default [
+  footerMessages,
+  appMessages,
+];
