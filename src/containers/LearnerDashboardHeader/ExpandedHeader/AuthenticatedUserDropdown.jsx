766f34553dc46ff20ab731d6032ec6409d73fc2b
Mateusz Kalisch <mateusz.kalisch@emag.lukasiewicz.gov.pl>
2024-11-08 16:00:38 +0000

    Telemetry link correction

--- src/containers/LearnerDashboardHeader/ExpandedHeader/AuthenticatedUserDropdown.jsx
+++ src/containers/LearnerDashboardHeader/ExpandedHeader/AuthenticatedUserDropdown.jsx
@@ -61,7 +61,7 @@ export const AuthenticatedUserDropdown = () => {
           {authenticatedUser?.administrator && (<Dropdown.Item href={`${getConfig().ACCOUNT_SETTINGS_URL.replace('account', 'demo')}`}>
             {formatMessage(messages.telemetryAdmin)}
           </Dropdown.Item>)}
-          {authenticatedUser?.administrator === false && (<Dropdown.Item href={`${getConfig().ACCOUNT_SETTINGS_URL.replace('account', 'demo')}details??page=inf`}>
+          {authenticatedUser?.administrator === false && (<Dropdown.Item href={`${getConfig().ACCOUNT_SETTINGS_URL.replace('account', 'demo')}?page=inf`}>
             {formatMessage(messages.telemetryUser)}
           </Dropdown.Item>)}
           {getConfig().ORDER_HISTORY_URL && (
