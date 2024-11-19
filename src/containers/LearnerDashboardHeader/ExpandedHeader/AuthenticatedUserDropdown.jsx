8d8e73669de83b91915027c888639dbdade26384
Mateusz Kalisch <mateusz.kalisch@emag.lukasiewicz.gov.pl>
2024-11-19 06:27:15 +0000

    Restored telemetry menu items

--- src/containers/LearnerDashboardHeader/ExpandedHeader/AuthenticatedUserDropdown.jsx
+++ src/containers/LearnerDashboardHeader/ExpandedHeader/AuthenticatedUserDropdown.jsx
@@ -58,12 +58,12 @@ export const AuthenticatedUserDropdown = () => {
           <Dropdown.Item href={getConfig().ACCOUNT_SETTINGS_URL}>
             {formatMessage(messages.account)}
           </Dropdown.Item>
-          {/* <Dropdown.Item href={`${getConfig().ACCOUNT_SETTINGS_URL.replace('account', 'dashboard')}?page=inf`}>
+          <Dropdown.Item href={`${getConfig().ACCOUNT_SETTINGS_URL.replace('account', 'dashboard')}?page=inf`}>
             {formatMessage(messages.telemetryUser)}
           </Dropdown.Item>
           {authenticatedUser?.administrator && (<Dropdown.Item href={`${getConfig().ACCOUNT_SETTINGS_URL.replace('account', 'dashboard')}`}>
             {formatMessage(messages.telemetryAdmin)}
-          </Dropdown.Item>)} */}
+          </Dropdown.Item>)}
           {/* {getConfig().ORDER_HISTORY_URL && (
             <Dropdown.Item href={getConfig().ORDER_HISTORY_URL}>
               {formatMessage(messages.orderHistory)}
