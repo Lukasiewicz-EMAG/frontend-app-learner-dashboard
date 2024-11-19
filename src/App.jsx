8c9cd359c52db586cd68e0601e5f986603ed1d15
Dawid Leśniak <dawid.lesniak@emag.lukasiewicz.gov.pl>
2024-11-19 12:13:19 +0100

    Add decorator

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ export const App = () => {
       <div>
         <AppWrapper>
           <LearnerDashboardHeader />
-          <main>
+          <main className="top-decoration">
             {hasNetworkFailure
               ? (
                 <Alert variant="danger">
